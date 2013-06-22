ac78321be3129db79f0b7ee3667d86f06961f9f2
Lee <lee@kudosknowledge.com>
2013-06-22 21:28:48 +1000

    Init source files

---
+++ prime.js
@@ -0,0 +1,18 @@
+function getPrimes(upToNumber) 
+	{
+	    var sieve = [], i, j, primes = [];
+	    for (i = 2; i <= upToNumber; ++i) 
+		{
+			if (!sieve[i]) 
+				{
+					primes.push(i); // i has not been marked -- it is prime
+					for (j = i << 1; j <= upToNumber; j += i) 
+						{
+							sieve[j] = true;
+					    	}
+				}
+		    }
+	    return primes;
+	}
+
+module.exports = getPrimes;
---
+++ run.js
@@ -0,0 +1,10 @@
+var fs = require("fs");
+var prime = require("./prime.js");
+var primeNumbers = prime( 100 );
+
+primeNumbers.forEach(
+	function( num )
+		{
+			console.log( num );
+		}
+);
